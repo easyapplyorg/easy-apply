38c264962c099677bc4ad15b876f6c2c256cc98a
SsebaddukaG <luckygilbert1993@gmail.com>
2019-11-20 00:51:07 -0600

    Created Company saving servlet

---
+++ src/edu/mum/cs/easyapply/servlets/CompanySavingServlet.java
@@ -0,0 +1,26 @@
+package edu.mum.cs.easyapply.servlets;
+
+import edu.mum.cs.easyapply.daos.company.CompanyDAO;
+import edu.mum.cs.easyapply.model.Company;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "ContactSavingServlet" , urlPatterns = {"/saving-company"})
+public class ContactSavingServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Company company = (Company) request.getAttribute("company");
+
+        if (company!=null)CompanyDAO.saveCompany(company);
+        System.out.println("Company saved");
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
